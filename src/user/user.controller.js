a45b8a3201a8122a35dbb53413ed8f3b3594b6be
fkstndnjs <fkstndnjs@naver.com>
2023-05-15 22:46:58 +0900

    feat: signup, login controller

---
+++ src/user/user.controller.js
@@ -0,0 +1,23 @@
+import express from "express";
+import * as userService from "./user.service.js";
+import { body } from "express-validator";
+import { validate } from "../middleware/validate.js";
+
+const userController = express.Router();
+
+const validateSignup = [
+  body("username").trim().notEmpty().withMessage("Username is required"),
+  body("password").trim().notEmpty().withMessage("Password is required"),
+  validate,
+];
+
+const validateLogin = [
+  body("username").trim().notEmpty().withMessage("Username is required"),
+  body("password").trim().notEmpty().withMessage("Password is required"),
+  validate,
+];
+
+userController.post("/signup", validateSignup, userService.signup);
+userController.post("/login", validateLogin, userService.login);
+
+export default userController;
