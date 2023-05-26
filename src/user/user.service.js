75bcc45e7ea67ad3c15f3ca3d6bd01c5a0090ff4
fkstndnjs <fkstndnjs@naver.com>
2023-05-26 12:15:52 +0900

    feat: createUserInfo Service

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -43,3 +43,38 @@ export async function login(req, res) {
 
   res.status(200).json({ success: true, message: "Login successful" });
 }
+
+export async function createUserInfo(req, res) {
+  try {
+    const { username, name, age, gender, height, objective } = req.body;
+
+    const user = await userRepository.findByUsername(username);
+
+    // 인증 체크
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    // 요청 형식 검사
+    if (!username || !name || !age || !gender || !height || !objective) {
+      return res.status(400).json({ success: false, message: "Bad Request" });
+    }
+
+    // 유저 정보 입력
+    await userRepository.createUserInfo({
+      username,
+      name,
+      age,
+      gender,
+      height,
+      objective,
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "User information created" });
+  } catch (error) {
+    console.error("Error creating user information:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
