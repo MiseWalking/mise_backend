a7b1f2d5176087b0ee90ff17eb6054f8a9951deb
fkstndnjs <fkstndnjs@naver.com>
2023-05-30 22:24:48 +0900

    chore: swagger

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,6 +1,62 @@
-import bcrypt from "bcrypt";
 import * as userRepository from "./user.repository.js";
 
+/**
+ * @swagger
+ *
+ * /signup:
+ *   post:
+ *     tags:
+ *      - User
+ *     summary: User signup
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       201:
+ *         description: Signup successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       409:
+ *         description: Username is already taken
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ */
 export async function signup(req, res) {
   const { username, password } = req.body;
 
@@ -22,6 +78,54 @@ export async function signup(req, res) {
   res.status(201).json({ message: "Signup successful" });
 }
 
+/**
+ * @swagger
+ *
+ * /login:
+ *   post:
+ *     tags:
+ *      - User
+ *     summary: User login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: true
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Invalid username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ */
 export async function login(req, res) {
   const { username, password } = req.body;
 
@@ -42,6 +146,90 @@ export async function login(req, res) {
   res.status(200).json({ success: true, message: "Login successful" });
 }
 
+/**
+ * @swagger
+ *
+ * /user-info:
+ *   post:
+ *     tags:
+ *      - User
+ *     summary: Create user information
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               age:
+ *                 type: number
+ *               gender:
+ *                 type: string
+ *               height:
+ *                 type: number
+ *               objective:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - name
+ *               - age
+ *               - gender
+ *               - height
+ *               - objective
+ *     responses:
+ *       201:
+ *         description: User information created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: true
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   default: false
+ *                 message:
+ *                   type: string
+ */
 export async function createUserInfo(req, res) {
   try {
     const { username, name, age, gender, height, objective } = req.body;
