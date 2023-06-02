fec833881faa0f940b3429063019cd612ba972e9
fkstndnjs <fkstndnjs@naver.com>
2023-06-02 22:49:46 +0900

    feat: 공공 데이터 포털 로직 추가

--- src/route/route.service.js
+++ src/route/route.service.js
@@ -1,4 +1,6 @@
 import * as routeRepository from "./route.repository.js";
+import { config } from "../../config.js";
+import axios from "axios";
 
 export const getRoute = async (req, res) => {
   try {
@@ -11,6 +13,73 @@ export const getRoute = async (req, res) => {
 
     const urls = images.map((image) => image.url);
 
+    //미세먼지
+    const key = config.seoulKey;
+    const { gu } = req.query;
+    const miseUrl = `http://openAPI.seoul.go.kr:8088/${key}/json/ListAirQualityByDistrictService/1/5/${gu}`;
+    const miseResponse = await axios.get(`${miseUrl}`);
+    const pm10 = miseResponse.data.ListAirQualityByDistrictService.row[0].PM10;
+
+    //날씨
+    const { date } = req.query;
+    const weatherQueryParams = new URLSearchParams({
+      serviceKey: config.weatherKey,
+      pageNo: 1,
+      numOfRows: 50,
+      dataType: "JSON",
+      base_date: date,
+      base_time: "1100",
+      nx: 55,
+      ny: 127,
+    });
+
+    const weatherUrl = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?${weatherQueryParams.toString()}`;
+
+    const weatherResponse = await axios.get(`${weatherUrl}`);
+
+    let weatherResult = {
+      TMP: "test",
+    };
+
+    weatherResponse.data.response.body.items.item.forEach((item) => {
+      if (item.category === "TMP") {
+        weatherResult.TMP = item.fcstValue;
+      }
+    });
+
+    //강수량
+    const gangQueryParams = new URLSearchParams({
+      serviceKey: config.weatherKey,
+      pageNo: 1,
+      numOfRows: 50,
+      dataType: "JSON",
+      base_date: date,
+      base_time: "1100",
+      nx: 55,
+      ny: 127,
+    });
+
+    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?${gangQueryParams.toString()}`;
+
+    const gangResponse = await axios.get(`${url}`);
+
+    let gangResult = {
+      POP: "test",
+      REH: "test",
+      PCP: "test",
+    };
+
+    gangResponse.data.response.body.items.item.forEach((item) => {
+      if (item.category === "POP") {
+        gangResult.POP = item.fcstValue;
+      } else if (item.category === "REH") {
+        gangResult.REH = item.fcstValue;
+      } else if (item.category === "PCP") {
+        gangResult.PCP = item.fcstValue;
+      }
+    });
+
+    console.log(pm10, weatherResult, gangResult);
     res.status(200).json({ success: true, urls });
   } catch (error) {
     console.error("Error retrieving images:", error);
