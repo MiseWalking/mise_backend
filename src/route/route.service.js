5afa51bb1e834f18bae1df408d792152239b4ce4
fkstndnjs <fkstndnjs@naver.com>
2023-06-03 11:20:57 +0900

    feat: 산책경로 추천 알고리즘즘

--- src/route/route.service.js
+++ src/route/route.service.js
@@ -2,6 +2,82 @@ import * as routeRepository from "./route.repository.js";
 import { config } from "../../config.js";
 import axios from "axios";
 
+/**
+ * @swagger
+ * /route/{imageId}:
+ *   get:
+ *     summary: Get route information
+ *     tags: [Route]
+ *     parameters:
+ *       - in: path
+ *         name: imageId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the image
+ *       - in: query
+ *         name: gu
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: District code
+ *       - in: query
+ *         name: date
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Date in the format YYYYMMDD
+ *     responses:
+ *       200:
+ *         description: Successful response with route information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 flag:
+ *                   type: string
+ *                   description: Flag indicating the route condition (좋음, 보통, 나쁨)
+ *                 routes:
+ *                   type: array
+ *                   description: List of recommended routes
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       url:
+ *                         type: string
+ *                         description: URL of the image
+ *                       recommended:
+ *                         type: boolean
+ *                         description: Indicates if the route is recommended
+ *       404:
+ *         description: Image not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
+
 export const getRoute = async (req, res) => {
   try {
     const { imageId } = req.params;
@@ -79,14 +155,87 @@ export const getRoute = async (req, res) => {
       }
     });
 
-    console.log(pm10, weatherResult, gangResult);
-    res.status(200).json({ success: true, urls });
+    // 산책 경로 추천
+    let recommendedRoutes = [];
+
+    let flag = "";
+
+    if (pm10 <= 30 && gangResult.POP <= 30 && weatherResult.TMP >= 20) {
+      flag = "좋음";
+    } else if (pm10 <= 50 && gangResult.POP <= 50 && weatherResult.TMP >= 15) {
+      flag = "보통";
+    } else {
+      flag = "나쁨";
+    }
+
+    urls.forEach((url) => {
+      let route = { url };
+      if (
+        (flag === "좋음" && url.includes("_3")) ||
+        (flag === "보통" && url.includes("_2")) ||
+        (flag === "나쁨" && url.includes("_1"))
+      ) {
+        route.recommended = true;
+      } else {
+        route.recommended = false;
+      }
+      recommendedRoutes.push(route);
+    });
+
+    res.status(200).json({ success: true, flag, routes: recommendedRoutes });
   } catch (error) {
     console.error("Error retrieving images:", error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
+/**
+ * @swagger
+ * /route:
+ *   post:
+ *     summary: Create a new route
+ *     tags: [Route]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               url:
+ *                 type: string
+ *                 description: URL of the image
+ *               imageId:
+ *                 type: integer
+ *                 description: ID of the image
+ *     responses:
+ *       201:
+ *         description: Successful response with the ID of the new image
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 imageId:
+ *                   type: integer
+ *                   description: ID of the new image
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ */
 export const createRoute = async (req, res) => {
   try {
     const { url, imageId } = req.body;
