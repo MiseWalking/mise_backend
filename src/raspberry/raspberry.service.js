b57cc280b9ce91e37a73389f08edfa90fc49608a
cpplovelove <gsgslolo@naver.com>
2023-06-05 15:39:51 +0900

    ras 수정

--- src/raspberry/raspberry.service.js
+++ src/raspberry/raspberry.service.js
@@ -12,11 +12,12 @@ export const getRas = async (req, res) => {
   });
 
   device.on("connect", function () {
+    console.log("connect");
     device.subscribe("sensor");
   });
 
   device.on("message", function (topic, payload) {
-    console.log("들어옴");
     console.log("message", topic, payload.toString());
+    return res.status(200).send(payload.toString());
   });
 };
